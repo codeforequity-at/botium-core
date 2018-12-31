851907f2ce375af2bd76bfef5a558fba31d6fab8
stefan-sevelda <stefan.sevelda@botium.at>
2018-12-31 14:37:27 +0100

    Bot-41: cleaned up unit test

--- test/scripting/scriptingProvider.spec.js
+++ test/scripting/scriptingProvider.spec.js
@@ -1,18 +1,18 @@
 const assert = require('chai').assert
 const ScriptingProvider = require('../../src/scripting/ScriptingProvider')
 
-describe('validate if valid asserterType', function () {
+describe('scriptingProvider._isValidAsserterType', function () {
   it('valid asserterType', async function () {
     let scriptingProvider = new ScriptingProvider()
     assert.equal(scriptingProvider._isValidAsserterType('assertConvoStep'), true)
   })
-  it('valid asserterType', async function () {
+  it('invalid asserterType', async function () {
     let scriptingProvider = new ScriptingProvider()
     assert.equal(scriptingProvider._isValidAsserterType('assertStep'), false)
   })
 })
 
-describe('exchange variable with value in asserterArgs', function () {
+describe('scriptingProvider._addScriptingMemoryToArgs', function () {
   it('exchange var with real value', async function () {
     let scriptingProvider = new ScriptingProvider()
     let asserter = {
@@ -40,6 +40,21 @@ describe('exchange variable with value in asserterArgs', function () {
     }
     let scriptingMemory = {
       '$count': '5'
+    }
+    assert.notEqual(scriptingProvider._addScriptingMemoryToArgs(asserter, scriptingMemory).args[1], 5)
+  })
+  it('different value', async function () {
+    let scriptingProvider = new ScriptingProvider()
+    let asserter = {
+      'name': 'DUMMY',
+      'args': [
+        'dbUrl',
+        '$count',
+        "INSERT INTO dummy(name, birthday) VALUES ('Max Mustermann', 1991-03-26);"
+      ]
+    }
+    let scriptingMemory = {
+      '$count': '4'
     }
     assert.notEqual(scriptingProvider._addScriptingMemoryToArgs(asserter, scriptingMemory).args[1], 5)
   })
