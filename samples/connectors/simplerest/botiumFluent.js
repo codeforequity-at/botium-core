08a9e1a2c22047f54e680ae53621f3e8857f732a
treml <ftreml@gmail.com>
2019-01-22 12:12:26 +0100

    New SimpleRest sample (with I am Botium sample bot)

--- samples/connectors/simplerest/botiumFluent.js
+++ samples/connectors/simplerest/botiumFluent.js
@@ -1,37 +1,13 @@
-const BotDriver = require('../../index').BotDriver
-const Capabilities = require('../../index').Capabilities
-
-function assert (expected, actual) {
-  if (!actual || actual.indexOf(expected) < 0) {
-    console.log(`ERROR: Expected <${expected}>, got <${actual}>`)
-  } else {
-    console.log(`SUCCESS: Got Expected <${expected}>`)
-  }
-}
+const BotDriver = require('../../../index').BotDriver
 
 const driver = new BotDriver()
-  .setCapability(Capabilities.PROJECTNAME, 'Simple Rest Sample')
-  .setCapability(Capabilities.CONTAINERMODE, 'simplerest')
-  .setCapability(Capabilities.SIMPLEREST_INIT_CONTEXT, { 'conversation_id': 'none' })
-  .setCapability(Capabilities.SIMPLEREST_INIT_TEXT, 'Hallo')
-  .setCapability(Capabilities.SIMPLEREST_URL, 'https://drei-chatapp-agent-website-tomcat.eu-de.mybluemix.net/api/v1/{{context.conversation_id}}/{{msg.messageText}}')
-  .setCapability(Capabilities.SIMPLEREST_METHOD, 'GET')
-  .setCapability(Capabilities.SIMPLEREST_HEADERS_TEMPLATE, { 'apiToken': 'testapitoken' })
-  .setCapability(Capabilities.SIMPLEREST_CONTEXT_JSONPATH, '$')
-  .setCapability(Capabilities.SIMPLEREST_RESPONSE_JSONPATH, '$.text.*')
-  .setCapability(Capabilities.SIMPLEREST_BUTTONS_JSONPATH, '$.quick_response.*')
 
 driver.BuildFluent()
   .Start()
-  .UserSaysText('Wer bist du ?')
-  .WaitBotSays((msg) => {
-    console.log(JSON.stringify(msg, null, 2))
-    assert('Ich bin ein virtueller Berater', msg.messageText)
-  })
-  .UserSaysText('Was ist Roaming ?')
-  .WaitBotSays((msg) => {
-    console.log(JSON.stringify(msg, null, 2))
-  })
+  .UserSaysText('Hello')
+  .WaitBotSaysText(console.log)
+  .UserSaysText('How are you ?')
+  .WaitBotSaysText(console.log)
   .Stop()
   .Clean()
   .Exec()
