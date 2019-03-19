f3ef664359135badbe3e0d4b5d464f8ae354a428
Attila Ujj <attila.ujj@botium.at>
2019-03-19 07:10:52 +0100

    SetScriptingMemoryLogicHook.js globalized

--- src/scripting/logichook/logichooks/SetScriptingMemoryLogicHook.js
+++ src/scripting/logichook/logichooks/SetScriptingMemoryLogicHook.js
@@ -1,35 +1,43 @@
 const debug = require('debug')('botium-SetScriptingMemoryLogicHook')
+const util = require('util')
 
 module.exports = class SetScriptingMemoryLogicHook {
-  constructor (context, caps = {}) {
+  constructor (context, caps = {}, globalArgs = {}) {
     this.context = context
     this.caps = caps
+    this.globalArgs = globalArgs
   }
 
-  onConvoBegin ({ scriptingMemory, convoStep, args }) {
-    return this._setScriptingMemory(scriptingMemory, convoStep, args)
+  onConvoBegin ({ scriptingMemory, convoStep, args, isGlobal }) {
+    return this._setScriptingMemory(scriptingMemory, convoStep, args, isGlobal, 'onConvoBegin')
   }
 
-  onMeEnd ({ scriptingMemory, convoStep, args }) {
-    return this._setScriptingMemory(scriptingMemory, convoStep, args)
+  onMeEnd ({ scriptingMemory, convoStep, args, isGlobal }) {
+    return this._setScriptingMemory(scriptingMemory, convoStep, args, isGlobal, 'onMeEnd')
   }
 
-  onBotEnd ({ scriptingMemory, convoStep, args }) {
-    return this._setScriptingMemory(scriptingMemory, convoStep, args)
+  onBotEnd ({ scriptingMemory, convoStep, args, isGlobal }) {
+    return this._setScriptingMemory(scriptingMemory, convoStep, args, isGlobal, 'onMeEnd')
   }
 
-  _setScriptingMemory (scriptingMemory, convoStep, args) {
+  _setScriptingMemory (scriptingMemory, convoStep, args, isGlobal, type) {
     if (args && args.length > 2) {
-      return Promise.reject(new Error(`${convoStep.stepTag}: SetScriptingMemoryLogicHook Too much argument "${args}"`))
-    }
-    if (args && args.length < 2) {
-      return Promise.reject(new Error(`${convoStep.stepTag}: SetScriptingMemoryLogicHook Not enough argument "${args}"`))
+      return Promise.reject(new Error(`${convoStep.stepTag}: SetScriptingMemoryLogicHook Too much argument ${util.inspect(args)}`))
     }
 
+    let name = args[0] || this.globalArgs.name
+    if (!name) {
+      return Promise.reject(new Error(`${convoStep.stepTag}: SetScriptingMemoryLogicHook Name is missing. args: ${util.inspect(args)}, \nglobalArgs: ${util.inspect(this.globalArgs)}`))
+    }
+
+    let value = args[1] || this.globalArgs.value
+    if (!value) {
+      return Promise.reject(new Error(`${convoStep.stepTag}: SetScriptingMemoryLogicHook Value is missing. args: ${util.inspect(args)}, \nglobalArgs: ${util.inspect(this.globalArgs)}`))
+    }
     // args[0] cant have the whole name of the variable, because the variable names are replaced
-    const name = '$' + args[0]
-    debug(`Set scripting memory variable "${name}" from "${scriptingMemory[name]}" to "${args[1]}"`)
-    scriptingMemory[name] = args[1]
+    name = '$' + name
+    debug(`Set scripting memory variable "${name}" from "${scriptingMemory[name]}" to "${value}, isGlobal: ${isGlobal}, type: ${type}"`)
+    scriptingMemory[name] = value
 
     return Promise.resolve()
   }
