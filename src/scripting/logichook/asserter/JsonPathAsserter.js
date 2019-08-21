c5d7fb3c3983818442cd86be55a9c89e7e0a0207
tremlfl <florian.treml@drei.com>
2019-08-21 11:23:05 +0200

    BOT-717 added option to just check existence of jsonpath

--- src/scripting/logichook/asserter/JsonPathAsserter.js
+++ src/scripting/logichook/asserter/JsonPathAsserter.js
@@ -8,33 +8,23 @@ module.exports = class JsonPathAsserter {
   }
 
   assertConvoStep ({ convo, convoStep, args, botMsg }) {
-    const rawBotResponse = botMsg.sourceData
-    const path = args[0]
-    const expected = args[1]
-    const [ actual ] = jsonPath.query(rawBotResponse, path)
-    if (expected && !this.context.Match(actual, expected)) {
-      return Promise.reject(new BotiumError(`${convoStep.stepTag}: Expected: ${expected} in jsonPath ${path}"`,
+    if (!botMsg || !botMsg.sourceData) return Promise.resolve()
+    if (!args || args.length === 0 || args.length > 2) {
+      return Promise.reject(new BotiumError(`${convoStep.stepTag}: JsonPathAsserter 1 or 2 arguments expected "${args}"`),
         {
           type: 'asserter',
+          subtype: 'wrong parameters',
           source: 'JsonPathAsserter',
-          context: {
-            // effective arguments getting from constructor
-            constructor: {},
-            params: {
-              args,
-              botMsg
-            }
-          },
-          cause: {
-            expected,
-            actual,
-            path
-          }
+          cause: { args }
         }
-      ))
+      )
     }
 
-    if (!actual) {
+    const rawBotResponse = botMsg.sourceData
+    const path = args[0]
+
+    const jsonPathValues = jsonPath.query(rawBotResponse, path)
+    if (!jsonPathValues || jsonPathValues.length === 0) {
       return Promise.reject(new BotiumError(`${convoStep.stepTag}: Could not find any element in jsonPath ${path}"`,
         {
           type: 'asserter',
@@ -48,13 +38,39 @@ module.exports = class JsonPathAsserter {
             }
           },
           cause: {
-            expected,
-            actual,
+            expected: args.length > 1 ? args[1] : null,
+            actual: null,
             path
           }
         }
       ))
     }
+    if (args.length > 1) {
+      const [ actual ] = jsonPathValues
+      const expected = args[1]
+      if (!this.context.Match(actual, expected)) {
+        return Promise.reject(new BotiumError(`${convoStep.stepTag}: Expected: ${expected} in jsonPath ${path}"`,
+          {
+            type: 'asserter',
+            source: 'JsonPathAsserter',
+            context: {
+            // effective arguments getting from constructor
+              constructor: {},
+              params: {
+                args,
+                botMsg
+              }
+            },
+            cause: {
+              expected,
+              actual,
+              path
+            }
+          }
+        ))
+      }
+    }
+
     return Promise.resolve()
   }
 }
