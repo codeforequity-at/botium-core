b9f8861c63bc37ca4519bc8e540568120bd5ace4
ftreml <florian.treml@botium.at>
2020-02-27 17:35:20 +0100

    Download media files

--- src/scripting/logichook/userinput/MediaInput.js
+++ src/scripting/logichook/userinput/MediaInput.js
@@ -1,5 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+const request = require('request')
 const mime = require('mime-types')
-const { URL } = require('url')
+const url = require('url')
 
 module.exports = class MediaInput {
   constructor (context, caps = {}, globalArgs = {}) {
@@ -10,25 +13,63 @@ module.exports = class MediaInput {
 
   _getResolvedUri (uri, convoDir, convoFilename) {
     if (this.globalArgs && this.globalArgs.baseUri) {
-      return new URL(uri, this.globalArgs.baseUri).toString()
+      return new url.URL(uri, this.globalArgs.baseUri)
     } else if (convoDir && convoFilename) {
-      return new URL(uri, `file://${convoDir}/${convoFilename}`).toString()
+      const basePath = path.resolve(convoDir)
+      return new url.URL(uri, `file://${basePath}/${convoFilename}`)
     } else {
-      return uri
+      return new url.URL(uri)
     }
   }
 
-  setUserInput ({ convoStep, args, meMsg, convo }) {
+  async _downloadMedia (uri) {
+    if (this.globalArgs && this.globalArgs.downloadMedia) {
+      if (uri.protocol === 'file:') {
+        try {
+          return fs.readFileSync(uri)
+        } catch (err) {
+          throw new Error(`downloadMedia failed: ${err.message}`)
+        }
+      } else if (uri.protocol === 'http:' || uri.protocol === 'https:') {
+        return new Promise((resolve, reject) => {
+          request({
+            uri: uri.toString(),
+            method: 'GET',
+            followAllRedirects: true,
+            encoding: null,
+            timeout: this.globalArgs.downloadTimeout || 10000
+          }, (err, response, body) => {
+            if (err) {
+              reject(new Error(`downloadMedia failed: ${err.message}`))
+            } else {
+              if (response.statusCode >= 400) {
+                return reject(new Error(`downloadMedia failed: ${response.statusCode}/${response.statusMessage}`))
+              }
+              resolve(body)
+            }
+          })
+        })
+      }
+    }
+  }
+
+  async setUserInput ({ convoStep, args, meMsg, convo }) {
     if (!args || args.length === 0 || args.length > 1) {
       return Promise.reject(new Error(`${convoStep.stepTag}: MediaInput requires exactly 1 argument`))
     }
     if (!meMsg.media) {
       meMsg.media = []
     }
-    meMsg.media.push({
-      mediaUri: this._getResolvedUri(args[0], convo.sourceTag.convoDir, convo.sourceTag.filename),
-      mimeType: mime.lookup(args[0])
-    })
-    return Promise.resolve()
+    try {
+      const uri = this._getResolvedUri(args[0], convo.sourceTag.convoDir, convo.sourceTag.filename)
+      const buffer = await this._downloadMedia(uri)
+      meMsg.media.push({
+        mediaUri: uri.toString(),
+        mimeType: mime.lookup(args[0]),
+        buffer
+      })
+    } catch (err) {
+      throw new Error(err.message)
+    }
   }
 }
--- test/scripting/userinputs/mediaInputConvos.spec.js
+++ test/scripting/userinputs/mediaInputConvos.spec.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const assert = require('chai').assert
+const nock = require('nock')
 const BotDriver = require('../../../').BotDriver
 const Capabilities = require('../../../').Capabilities
 
@@ -44,13 +45,13 @@ describe('scripting.userinputs.mediaInputConvos.relative', function () {
     assert.equal(this.compiler.convos[0].conversation[0].userInputs.length, 1)
     assert.equal(this.compiler.convos[0].conversation[0].userInputs[0].name, 'MEDIA')
     assert.equal(this.compiler.convos[0].conversation[0].userInputs[0].args.length, 1)
-    assert.equal(this.compiler.convos[0].conversation[0].userInputs[0].args[0], 'test.jpg')
+    assert.equal(this.compiler.convos[0].conversation[0].userInputs[0].args[0], 'botium.png')
 
     const transcript = await this.compiler.convos[0].Run(this.container)
     assert.equal(transcript.steps.length, 1)
     assert.equal(transcript.steps[0].actual.media.length, 1)
-    assert.isTrue(transcript.steps[0].actual.media[0].mediaUri.endsWith('test/scripting/userinputs/convos/test.jpg'))
-    assert.equal(transcript.steps[0].actual.media[0].mimeType, 'image/jpeg')
+    assert.isTrue(transcript.steps[0].actual.media[0].mediaUri.endsWith('test/scripting/userinputs/convos/botium.png'))
+    assert.equal(transcript.steps[0].actual.media[0].mimeType, 'image/png')
   })
 
   it('should add multi media in user message', async function () {
@@ -97,13 +98,13 @@ describe('scripting.userinputs.mediaInputConvos.baseUri', function () {
     assert.equal(this.compiler.convos[0].conversation[0].userInputs.length, 1)
     assert.equal(this.compiler.convos[0].conversation[0].userInputs[0].name, 'MEDIA')
     assert.equal(this.compiler.convos[0].conversation[0].userInputs[0].args.length, 1)
-    assert.equal(this.compiler.convos[0].conversation[0].userInputs[0].args[0], 'test.jpg')
+    assert.equal(this.compiler.convos[0].conversation[0].userInputs[0].args[0], 'botium.png')
 
     const transcript = await this.compiler.convos[0].Run(this.container)
     assert.equal(transcript.steps.length, 1)
     assert.equal(transcript.steps[0].actual.media.length, 1)
-    assert.equal(transcript.steps[0].actual.media[0].mediaUri, 'https://www.botium.at/test.jpg')
-    assert.equal(transcript.steps[0].actual.media[0].mimeType, 'image/jpeg')
+    assert.equal(transcript.steps[0].actual.media[0].mediaUri, 'https://www.botium.at/botium.png')
+    assert.equal(transcript.steps[0].actual.media[0].mimeType, 'image/png')
   })
 
   it('should add multi media in user message', async function () {
@@ -116,5 +117,85 @@ describe('scripting.userinputs.mediaInputConvos.baseUri', function () {
     assert.equal(transcript.steps[0].actual.media[0].mimeType, 'image/jpeg')
     assert.equal(transcript.steps[0].actual.media[1].mediaUri, 'https://www.botium.at/test2.jpg')
     assert.equal(transcript.steps[0].actual.media[1].mimeType, 'image/jpeg')
+  })
+})
+
+describe('scripting.userinputs.mediaInputDownloadConvos.relative', function () {
+  beforeEach(async function () {
+    const myCaps = {
+      [Capabilities.PROJECTNAME]: 'scripting.userinputs.mediaInputDownloadConvos',
+      [Capabilities.CONTAINERMODE]: echoConnector,
+      [Capabilities.SCRIPTING_ENABLE_MEMORY]: true,
+      [Capabilities.USER_INPUTS]: [
+        {
+          ref: 'MEDIA',
+          src: 'MediaInput',
+          args: {
+            downloadMedia: true
+          }
+        }
+      ]      
+    }
+    const driver = new BotDriver(myCaps)
+    this.compiler = driver.BuildCompiler()
+    this.container = await driver.Build()
+  })
+  afterEach(async function () {
+    this.container && await this.container.Clean()
+  })
+
+  it('should add media buffer in user message', async function () {
+    this.compiler.ReadScript(path.resolve(__dirname, 'convos'), 'media.convo.txt')
+
+    const transcript = await this.compiler.convos[0].Run(this.container)
+    assert.equal(transcript.steps.length, 1)
+    assert.equal(transcript.steps[0].actual.media.length, 1)
+    assert.isTrue(transcript.steps[0].actual.media[0].mediaUri.endsWith('test/scripting/userinputs/convos/botium.png'))
+    assert.equal(transcript.steps[0].actual.media[0].mimeType, 'image/png')
+    assert.isNotNull(transcript.steps[0].actual.media[0].buffer)
+  })
+})
+
+describe('scripting.userinputs.mediaInputDownloadConvos.baseUri', function () {
+  beforeEach(async function () {
+    const myCaps = {
+      [Capabilities.PROJECTNAME]: 'scripting.userinputs.mediaInputDownloadConvos',
+      [Capabilities.CONTAINERMODE]: echoConnector,
+      [Capabilities.SCRIPTING_ENABLE_MEMORY]: true,
+      [Capabilities.USER_INPUTS]: [
+        {
+          ref: 'MEDIA',
+          src: 'MediaInput',
+          args: {
+            downloadMedia: true,
+            baseUri: 'https://www.botium.at'
+          }
+        }
+      ]
+    }
+    const driver = new BotDriver(myCaps)
+    this.compiler = driver.BuildCompiler()
+    this.container = await driver.Build()
+  })
+  afterEach(async function () {
+    this.container && await this.container.Clean()
+  })
+
+  it('should add media in user message', async function () {
+    const scope = nock('https://www.botium.at')
+      .get('/botium.png')
+      .reply(200, Buffer.from('hello world'))
+      .persist()
+
+    this.compiler.ReadScript(path.resolve(__dirname, 'convos'), 'media.convo.txt')
+
+    const transcript = await this.compiler.convos[0].Run(this.container)
+    assert.equal(transcript.steps.length, 1)
+    assert.equal(transcript.steps[0].actual.media.length, 1)
+    assert.equal(transcript.steps[0].actual.media[0].mediaUri, 'https://www.botium.at/botium.png')
+    assert.equal(transcript.steps[0].actual.media[0].mimeType, 'image/png')
+    assert.isNotNull(transcript.steps[0].actual.media[0].buffer)
+
+    scope.persist(false)
   })
 })
