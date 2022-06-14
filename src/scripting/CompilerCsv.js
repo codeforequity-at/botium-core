cf01983b79c7eeb0b93b56de75d742a986292e07
Attila Ujj <attila.ujj@botium.at>
2022-06-14 09:07:51 +0200

    BOT-3075 CSV caps (for livepersion) added

--- src/scripting/CompilerCsv.js
+++ src/scripting/CompilerCsv.js
@@ -47,6 +47,7 @@ module.exports = class CompilerCsv extends CompilerBase {
     if (scriptData.length === 0) {
       return []
     }
+    const legacyModeOn = !this._GetOptionalCapability(Capabilities.SCRIPTING_CSV_LEGACY_MODE_OFF, false)
 
     let delimiter = this._GetOptionalCapability(Capabilities.SCRIPTING_CSV_DELIMITER)
     if (!delimiter) {
@@ -71,7 +72,8 @@ module.exports = class CompilerCsv extends CompilerBase {
         delimiter,
         escape: this.caps[Capabilities.SCRIPTING_CSV_ESCAPE],
         quote: this.caps[Capabilities.SCRIPTING_CSV_QUOTE],
-        columns: false
+        columns: false,
+        relax_column_count: true
       })
     } catch (err) {
       throw new Error(`Invalid CSV: ${err.message || err}`)
@@ -79,123 +81,170 @@ module.exports = class CompilerCsv extends CompilerBase {
     if (rows.length === 0) {
       return []
     }
-    if (rows[0].length === 1) {
-      debug('Found 1-column CSV file, treating it as utterance file')
-      if (scriptType === Constants.SCRIPTING_TYPE_UTTERANCES) {
-        const result = [{ name: rows[0][0], utterances: rows.slice(1).map(r => r[0]) }]
-        this.context.AddUtterances(result)
-        return result
-      } else {
+    const columnCount = Math.max(...rows.map(row => row.length))
+
+    const parseUtterances = () => {
+      const result = []
+      const startRow = this._GetOptionalCapability(Capabilities.SCRIPTING_CSV_UTTERANCE_STARTROW, 2) - 1
+      const startRowHeader = this._GetOptionalCapability(Capabilities.SCRIPTING_CSV_UTTERANCE_STARTROW_HEADER)
+      for (let col = 0; col < columnCount; col++) {
+        const uttStruct = {
+          name: rows[0][col],
+          utterances: []
+        }
+        let skip = startRowHeader ? true : false
+        for (let row = startRow; row < rows.length && (skip || !!rows[row][col]); row++) {
+          if (!skip) {
+            uttStruct.utterances.push(rows[row][col])
+          } else {
+            if (startRowHeader === rows[row][col]) {
+              skip = false
+            }
+          }
+        }
+        result.push(uttStruct)
+      }
+
+      this.context.AddUtterances(result)
+
+    }
+    if ((scriptType === Constants.SCRIPTING_TYPE_CONVO || scriptType === Constants.SCRIPTING_TYPE_PCONVO)) {
+      if (columnCount === 1 || (!legacyModeOn && columnCount > 3)) {
         return []
       }
-    }
-
-    if (scriptType !== Constants.SCRIPTING_TYPE_CONVO && scriptType !== Constants.SCRIPTING_TYPE_PCONVO) {
-      return []
-    }
-    let header = null
-    if (rows.length > 0 && this.caps[Capabilities.SCRIPTING_CSV_SKIP_HEADER]) {
-      header = rows[0]
-      rows = rows.slice(1)
-    }
-    if (rows.length === 0) {
-      return []
-    }
-
-    const lineNumberBase = this.caps[Capabilities.SCRIPTING_CSV_SKIP_HEADER] ? 2 : 1
-    if (rows[0].length === 2) {
-      debug('Found 2-column CSV file, treating it as question/answer file')
-
-      let colQuestion = DEFAULT_QA_COLUMN_QUESTION
-      let colAnswer = DEFAULT_QA_COLUMN_ANSWER
-
-      if (header) {
-        if (this.caps[Capabilities.SCRIPTING_CSV_QA_COLUMN_QUESTION] !== undefined) {
-          colQuestion = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_QA_COLUMN_QUESTION])
-        }
-        if (this.caps[Capabilities.SCRIPTING_CSV_QA_COLUMN_ANSWER] !== undefined) {
-          colAnswer = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_QA_COLUMN_ANSWER])
-        }
+      let header = null
+      if (rows.length > 0 && this.caps[Capabilities.SCRIPTING_CSV_SKIP_HEADER]) {
+        header = rows[0]
+        rows = rows.slice(1)
+      }
+      if (rows.length === 0) {
+        return []
       }
 
-      const convos = rows.map((row, i) => new Convo(this.context, {
-        header: {
-          name: `L${i + lineNumberBase}`
-        },
-        conversation: [
-          Object.assign({},
-            linesToConvoStep(
-              [row[colQuestion]],
-              'me',
-              this.context,
-              undefined,
-              true
-            ), { stepTag: `L${i + lineNumberBase}-Question` }),
-          Object.assign({},
-            linesToConvoStep(
-              [row[colAnswer]],
-              'bot',
-              this.context,
-              undefined,
-              true
-            ), { stepTag: `L${i + lineNumberBase}-Answer` })
-        ]
-      }))
-      if (scriptType === Constants.SCRIPTING_TYPE_CONVO) {
-        this.context.AddConvos(convos)
-      } else if (scriptType === Constants.SCRIPTING_TYPE_PCONVO) {
-        this.context.AddPartialConvos(convos)
-      }
-      return convos
-    }
+      const lineNumberBase = this.caps[Capabilities.SCRIPTING_CSV_SKIP_HEADER] ? 2 : 1
+      if (columnCount === 2) {
+        debug('Found 2-column CSV file, treating it as question/answer file')
 
-    if (rows[0].length >= 3) {
-      debug('Found 3-column CSV file, treating it as multi-row conversation file')
+        let colQuestion = DEFAULT_QA_COLUMN_QUESTION
+        let colAnswer = DEFAULT_QA_COLUMN_ANSWER
 
-      let colConversationId = DEFAULT_MULTIROW_COLUMN_CONVERSATION
-      let colSender = DEFAULT_MULTIROW_COLUMN_SENDER
-      let colText = DEFAULT_MULTIROW_COLUMN_TEXT
-
-      if (header) {
-        if (this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_CONVERSATION_ID] !== undefined) {
-          colConversationId = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_CONVERSATION_ID])
+        if (header) {
+          if (this.caps[Capabilities.SCRIPTING_CSV_QA_COLUMN_QUESTION] !== undefined) {
+            colQuestion = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_QA_COLUMN_QUESTION])
+          }
+          if (this.caps[Capabilities.SCRIPTING_CSV_QA_COLUMN_ANSWER] !== undefined) {
+            colAnswer = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_QA_COLUMN_ANSWER])
+          }
         }
-        if (this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_SENDER] !== undefined) {
-          colSender = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_SENDER])
-        }
-        if (this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_TEXT] !== undefined) {
-          colText = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_TEXT])
-        }
-      }
 
-      const conversationIds = _.uniq(rows.map(r => r[colConversationId]))
-      const convos = conversationIds.map(conversationId => {
-        const convoRows = rows.map((row, i) => {
-          if (row[colConversationId] === conversationId) {
-            return Object.assign({},
+        const convos = rows.map((row, i) => new Convo(this.context, {
+          header: {
+            name: `L${i + lineNumberBase}`
+          },
+          conversation: [
+            Object.assign({},
               linesToConvoStep(
-                [row[colText]],
-                row[colSender],
+                [row[colQuestion]],
+                'me',
                 this.context,
                 undefined,
                 true
-              ), { stepTag: `L${i + lineNumberBase}` })
-          }
-          return null
-        }).filter(c => c)
-        return new Convo(this.context, {
-          header: {
-            name: conversationId
-          },
-          conversation: convoRows
-        })
-      })
-      if (scriptType === Constants.SCRIPTING_TYPE_CONVO) {
-        this.context.AddConvos(convos)
-      } else if (scriptType === Constants.SCRIPTING_TYPE_PCONVO) {
-        this.context.AddPartialConvos(convos)
+              ), { stepTag: `L${i + lineNumberBase}-Question` }),
+            Object.assign({},
+              linesToConvoStep(
+                [row[colAnswer]],
+                'bot',
+                this.context,
+                undefined,
+                true
+              ), { stepTag: `L${i + lineNumberBase}-Answer` })
+          ]
+        }))
+        if (scriptType === Constants.SCRIPTING_TYPE_CONVO) {
+          this.context.AddConvos(convos)
+        } else if (scriptType === Constants.SCRIPTING_TYPE_PCONVO) {
+          this.context.AddPartialConvos(convos)
+        }
+        return convos
       }
-      return convos
+
+      if (columnCount >= 3) {
+        debug('Found 3-column CSV file, treating it as multi-row conversation file')
+
+        let colConversationId = DEFAULT_MULTIROW_COLUMN_CONVERSATION
+        let colSender = DEFAULT_MULTIROW_COLUMN_SENDER
+        let colText = DEFAULT_MULTIROW_COLUMN_TEXT
+
+        if (header) {
+          if (this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_CONVERSATION_ID] !== undefined) {
+            colConversationId = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_CONVERSATION_ID])
+          }
+          if (this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_SENDER] !== undefined) {
+            colSender = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_SENDER])
+          }
+          if (this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_TEXT] !== undefined) {
+            colText = _findColIndex(header, this.caps[Capabilities.SCRIPTING_CSV_MULTIROW_COLUMN_TEXT])
+          }
+        }
+
+        const conversationIds = _.uniq(rows.map(r => r[colConversationId]))
+        const convos = conversationIds.map(conversationId => {
+          const convoRows = rows.map((row, i) => {
+            if (row[colConversationId] === conversationId) {
+              return Object.assign({},
+                linesToConvoStep(
+                  [row[colText]],
+                  row[colSender],
+                  this.context,
+                  undefined,
+                  true
+                ), { stepTag: `L${i + lineNumberBase}` })
+            }
+            return null
+          }).filter(c => c)
+          return new Convo(this.context, {
+            header: {
+              name: conversationId
+            },
+            conversation: convoRows
+          })
+        })
+        if (scriptType === Constants.SCRIPTING_TYPE_CONVO) {
+          this.context.AddConvos(convos)
+        } else if (scriptType === Constants.SCRIPTING_TYPE_PCONVO) {
+          this.context.AddPartialConvos(convos)
+        }
+        return convos
+      }
+    } else if (scriptType === Constants.SCRIPTING_TYPE_UTTERANCES) {
+      if (columnCount === 2 || columnCount === 3 || (legacyModeOn && columnCount > 4)) {
+        return []
+      }
+      const result = []
+      const startRow = this._GetOptionalCapability(Capabilities.SCRIPTING_CSV_UTTERANCE_STARTROW, 2) - 1
+      const startRowHeader = this._GetOptionalCapability(Capabilities.SCRIPTING_CSV_UTTERANCE_STARTROW_HEADER)
+      for (let col = 0; col < columnCount; col++) {
+        const uttStruct = {
+          name: rows[0][col],
+          utterances: []
+        }
+        let skip = startRowHeader ? true : false
+        for (let row = startRow; row < rows.length && (skip || !!rows[row][col]); row++) {
+          if (!skip) {
+            uttStruct.utterances.push(rows[row][col])
+          } else {
+            if (startRowHeader === rows[row][col]) {
+              skip = false
+            }
+          }
+        }
+        result.push(uttStruct)
+      }
+
+      this.context.AddUtterances(result)
+      return result
+    } else {
+      return []
     }
   }
 }
