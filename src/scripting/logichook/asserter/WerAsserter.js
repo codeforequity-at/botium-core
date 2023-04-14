5b7d350e0d3d3017456ec9bea6330845308abaf2
Szabolcs Gergye <szabolcs.gergye@cyara.com>
2023-04-14 15:25:19 +0200

    BOT-3713 CORE: Error message support for Testset Matching mode

--- src/scripting/logichook/asserter/WerAsserter.js
+++ src/scripting/logichook/asserter/WerAsserter.js
@@ -1,12 +1,61 @@
 // const _ = require('lodash')
 const { BotiumError } = require('../../BotiumError')
-const { calculateWer } = require('../../helper')
+const { calculateWer, toPercent } = require('../../helper')
 
 module.exports = class WerAsserter {
   constructor (context, caps = {}) {
     this.context = context
     this.caps = caps
     this.name = 'Word Error Rate Asserter'
+  }
+
+  assertNotConvoStep ({ convo, convoStep, args, botMsg }) {
+    if (!args || args.length < 1) {
+      return Promise.reject(new BotiumError(`${convoStep.stepTag}: ${this.name} - no argument given`,
+        {
+          type: 'asserter',
+          subtype: 'wrong parameters',
+          source: this.name,
+          cause: { args }
+        }
+      ))
+    }
+    if (args.length > 2) {
+      return Promise.reject(new BotiumError(`${convoStep.stepTag}: ${this.name} - too many arguments "${args}"`,
+        {
+          type: 'asserter',
+          subtype: 'wrong parameters',
+          source: this.name,
+          cause: { args }
+        }
+      ))
+    }
+
+    const utterance = args[0]
+    const threshold = ([',', '.'].find(p => `${args[1]}`.includes(p)) ? parseFloat(args[1]) : parseInt(args[1]) / 100).toFixed(2)
+
+    const wer = calculateWer(botMsg.messageText, utterance)
+
+    if (wer < threshold) {
+      return Promise.reject(new BotiumError(
+        `${convoStep.stepTag}: Word Error Rate (${toPercent(wer)}) lower than accepted (${toPercent(threshold)})`,
+        {
+          type: 'asserter',
+          source: this.name,
+          context: {
+            params: {
+              args
+            }
+          },
+          cause: {
+            expected: `>=${toPercent(threshold)} (${utterance})`,
+            actual: `${toPercent(wer)} (${botMsg.messageText})`
+          }
+        }
+      ))
+    }
+
+    return Promise.resolve()
   }
 
   assertConvoStep ({ convo, convoStep, args, botMsg }) {
@@ -37,10 +86,8 @@ module.exports = class WerAsserter {
     const wer = calculateWer(botMsg.messageText, utterance)
 
     if (wer > threshold) {
-      const _toPercent = (s) => `${(s * 100).toFixed(0)}%`
-
       return Promise.reject(new BotiumError(
-        `${convoStep.stepTag}: Word Error Rate (${_toPercent(wer)}) higher than accepted (${_toPercent(threshold)})`,
+        `${convoStep.stepTag}: Word Error Rate (${toPercent(wer)}) higher than accepted (${toPercent(threshold)})`,
         {
           type: 'asserter',
           source: this.name,
@@ -50,8 +97,8 @@ module.exports = class WerAsserter {
             }
           },
           cause: {
-            expected: `<=${_toPercent(threshold)}`,
-            actual: `${_toPercent(wer)}`
+            expected: `<=${toPercent(threshold)} (${utterance})`,
+            actual: `${toPercent(wer)} (${botMsg.messageText})`
           }
         }
       ))
